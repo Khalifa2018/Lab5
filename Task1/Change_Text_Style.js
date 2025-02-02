d5e5dc0030c5fe26d8fe5125d53aad54b5f4226d
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2025-02-02 20:17:51 +0200

    Add task 1 js

---
+++ Task1/Change_Text_Style.js
@@ -0,0 +1,44 @@
+function ChangeFont(newFont) {
+  var input = document.getElementById("PAR");
+  input.style.fontFamily = newFont;
+}
+
+function ChangeAlign(newAlign) {
+  var input = document.getElementById("PAR");
+  input.style.textAlign = newAlign;
+}
+
+function ChangeHeight(newHeight) {
+  var input = document.getElementById("PAR");
+  input.style.lineHeight = newHeight;
+}
+
+function ChangeLSpace(newLSpace) {
+  var input = document.getElementById("PAR");
+  input.style.letterSpacing = newLSpace;
+}
+
+function ChangeIndent(newIndent) {
+  var input = document.getElementById("PAR");
+  input.style.textIndent = newIndent;
+}
+
+function ChangeTransform(newTransform) {
+  var input = document.getElementById("PAR");
+  input.style.textTransform = newTransform;
+}
+
+function ChangeDecorate(newDecorate) {
+  var input = document.getElementById("PAR");
+  input.style.textDecoration = newDecorate;
+}
+
+function ChangeBorder(newBorder) {
+  var input = document.getElementById("PAR");
+  input.style.border = newBorder;
+}
+
+function ChangeBorderColor(newBorderColor) {
+  var input = document.getElementById("PAR");
+  input.style.borderColor = newBorderColor;
+}
